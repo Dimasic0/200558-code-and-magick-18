dccc7855f1163a804ce67d93b9d9b195d752be11
Дима <dmitriy.golovin.2001.03@mail.ru>
2019-09-17 15:08:59 +0300

    исправил ошибку.

--- js/stat.js
+++ js/stat.js
@@ -1,19 +1,19 @@
 'use strict';
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var BAR_WIDTH = 40;
-var BAR_HEIGHT = 150;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var BAR_X = 140;
-var BAR_Y = 100;
-var GAP = 10;
-var BAR_GAP = 50;
+var cloud_width = 420;
+var cloud_height = 270;
+var bar_width = 40;
+var bar_height= 150;
+var cloud_x = 100;
+var cloud_y = 10;
+var bar_x = 140;
+var bar_y = 100;
+var gap = 10;
+var bar_gap = 50;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(x, y, cloud_width, cloud_height);
 };
 
 var getTextValue = function (ctx, text, x, y, color) {
@@ -22,12 +22,12 @@ var getTextValue = function (ctx, text, x, y, color) {
   ctx.fillText(text, x, y);
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
+var getMaxElement = function (element) {
+  var maxElement = element[0];
 
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
+  for (var i = 1; i < element.length; i++) {
+    if (element[i] > maxElement) {
+      maxElement = element[i];
     }
   }
 
@@ -35,11 +35,11 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  var CLOUD_SHADOW_X = CLOUD_X + GAP;
-  var CLOUD_SHADOW_Y = CLOUD_Y + GAP;
+  var cloud_shadow_x = cloud_x + gap;
+  var cloud_shadow_y = cloud_y + gap;
 
-  renderCloud(ctx, CLOUD_SHADOW_X, CLOUD_SHADOW_Y, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, cloud_shadow_x, cloud_shadow_y, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, cloud_x, cloud_y, '#fff');
 
   getTextValue(ctx, 'Ура вы победили!', 120, 42, '#000000');
   getTextValue(ctx, 'Список результатов:', 120, 62, '#000000');
@@ -48,16 +48,15 @@ window.renderStatistics = function (ctx, names, times) {
 
   for (var i = 0; i < names.length; i++) {
     var playerName = names[i];
-    var PLAYER_NAME_X = BAR_X + (BAR_WIDTH + BAR_GAP) * i;
-    var PLAYER_NAME_Y = CLOUD_HEIGHT;
+    var playerNameX = bar_x + (bar_width + bar_gap) * i;
 
-    getTextValue(ctx, playerName, PLAYER_NAME_X, PLAYER_NAME_Y, '#000000');
+    getTextValue(ctx, playerName, playerNameX, cloud_height, '#000000');
 
     var playerScore = Math.round(times[i]);
-    var PLAYER_SCORE_X = BAR_X + (BAR_WIDTH + BAR_GAP) * i;
-    var PLAYER_SCORE_Y = CLOUD_HEIGHT - (BAR_HEIGHT * times[i] / maxTime) - GAP * 3;
+    var playerScoreX = bar_x + (bar_width + bar_gap) * i;
+    var playerScoreY = cloud_height - (bar_height * times[i] / maxTime) - gap * 3;
 
-    getTextValue(ctx, playerScore, PLAYER_SCORE_X, PLAYER_SCORE_Y, '#000000');
+    getTextValue(ctx, playerScore, playerScoreX, playerScoreY, '#000000');
 
     if (playerName === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
@@ -65,10 +64,10 @@ window.renderStatistics = function (ctx, names, times) {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
     }
 
-    var BAR_HEIGHT_WINNER = (BAR_HEIGHT * times[i] / maxTime) * -1;
-    var BAR_X_WINNER = BAR_X + (BAR_WIDTH + BAR_GAP) * i;
-    var BAR_Y_WINNER = BAR_Y + BAR_HEIGHT;
+    var barHeightWinner = (bar_height * times[i] / maxTime) * -1;
+    var barXWinner = bar_x + (bar_width + bar_gap) * i;
+    var barYWinner = bar_y + bar_height;
 
-    ctx.fillRect(BAR_X_WINNER, BAR_Y_WINNER, BAR_WIDTH, BAR_HEIGHT_WINNER);
+    ctx.fillRect(barXWinner, barYWinner, bar_width, barHeightWinner);
   }
 };
