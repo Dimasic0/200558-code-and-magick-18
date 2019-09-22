5003b40b52f3fc9e30d841efe7ebd8977ee486d7
Дима <dmitriy.golovin.2001.03@mail.ru>
2019-09-22 12:36:10 +0300

    Выполнил задание № 2.

--- js/setup.js
+++ js/setup.js
@@ -1,135 +1,56 @@
+'use strict';
+
 var SETUP_ITEM = document.querySelector(".setup");
 var SETUP_SIMILAR_ITEM = document.querySelector(".setup-similar-item");
 var fragment = document.createDocumentFragment();
 var randomName = 0;
 SETUP_ITEM.classList.remove("hidden");
-var wizards=[{name:"Иван"}];
+var wizards = [];
+var NAME_ITEM = [];
+var names = ["Иван", "Хуан Себастьян", "Мария", "Кристоф", "Виктор", "Юлия", "Люпита", "Вашингтон"];
+var lastNames = ["Марья", "Верон", "Мирабелла", "Вальц", "Онопко", "Топольницкая", "Нионго", "Ирвинг"];
+var COAT_COLORS = ["rgb(101, 137, 164)", "rgb(241, 43, 107)", "rgb(146, 100, 161)", "rgb(56, 159, 117)", "rgb(215, 210, 55)", "rgb(0, 0, 0)"];
+var EYE_COLOR = ["black", "red", "blue", "yellow", "green"];
+var similarWizardTemplate = document.querySelector("#similar-wizard-template").content.querySelector(".setup-similar-item");
+var elementSimilarList = document.querySelector(".setup-similar-list");
+var fragment = document.createDocumentFragment();
+var wizardElement = 0;
+var setupSimilar = document.querySelector(".setup-similar").classList.remove("hidden");
+
 function casual(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 
 }
-
-for (var i = 0; i <= 2; i++) {
-  randomName = casual(1, 9);
-  if (randomName == 1) {
-    wizards[i].name = "Иван";
-  }
-  if (randomName == 2) {
-    wizards[i].name = "Хуан Себастьян";
-  }
-  if (randomName == 3) {
-    wizards[i].name = "Мария";
-  }
-  if (randomName == 4) {
-    wizards.name = "Кристоф";
-  }
-  if (randomName == 5) {
-    wizards[i].name = "Виктор";
-  }
-  if (randomName == 6) {
-    wizards[i].name = "Юлия";
-  }
-  if (randomName == 7) {
-    wizards[i].name = "Люпита";
-  }
-  if (randomName == 8) {
-    wizards[i].name = "Вашингтон";
-  }
-}
-
-for (var i = 0; i <= 2; i++) {
-  randomName = casual(1, 8);
-
-  if (randomName == 1) {
-    wizards[i].surname = "Марья";
-  }
-  if (randomName == 2) {
-    wizards[i].surname = "Верон";
-  }
-  if (randomName == 3) {
-    wizards[i].surname = "Мирабелла";
-  }
-  if (randomName == 4) {
-    wizards[i].surname = "Вальц";
-  }
-  if (randomName == 5) {
-    wizards[i].surname = "Онопко";
-  }
-  if (randomName == 6) {
-    wizards[i].surname = "Топольницкая";
-  }
-  if (randomName == 7) {
-    wizards[i].surname = "Нионго";
-  }
-  if (randomName == 8) {
-    wizards[i].surname = "Ирвинг";
-  }
-}
-
-for(var i=0; i<=5; i++)
+wizards = [
   {
-    randomName=casual(1,6);
-    if(randomName==1)
-      {
-        wizards[i].coatColor="rgb (101, 137, 164)";
-      }
-    if(randomName==2)
-      {
-        wizards[i].coatColor="rgb (241, 43, 107)";
-      }
-    if(randomName==3)
-      {
-        wizards[i].coatColor="rgb (146, 100, 161)";
-      }
-    if(randomName==4)
-      {
-        wizards[i].coatColor="rgb (56, 159, 117)";
-      }
-      if(randomName==5)
-     {
-       wizards[i].coatColor="rgb (215, 210, 55)";
-     }
-    if(randomName==6)
-      {
-        wizards[i].coatColor="rgb (0, 0, 0)";
-      }
-
-  }
-
-for(var i=0; i<=4; i++)
-{
-   randomName=casual(1,6);
-  if(randomName==1)
-    {
-     wizards.eyesColor="black";
-    }
-   if(randomName==2)
-     {
-       wizards.eyesColor="red";
-     }
-  if(randomName==3)
-    {
-      wizards.eyesColor="blue";
-    }
-  if(randomName==4)
-   {
-     wizards.eyesColor="yellow";
-   }
-  if(randomName==5)
-    {
-      wizards.eyesColor="green";
-    }
-}
-
-console.log(wizards[0].name);
-console.log(wizards[0].surname);
-console.log(wizards.coatColor);
-console.log(wizards.eyesColor);
-/*for (var i=0; i<=5; i++)
+    name: names[casual(0, 7)] + " " + lastNames[casual(0, 7)],
+    coatColor: COAT_COLORS[casual(0, 5)],
+    eyesColor: EYE_COLOR[casual(0, 4)]
+  },
   {
-    var NAME_ITEM[i]=document.createElement("p");
-      NAME_ITEM[i].className="setup-similar-label";
-      NAME_ITEM[i].innerHTML=wizards[i].name;
-     SETUP_SIMILAR_ITEM.appendChild(NAME_ITEM[i]);
+    name: names[casual(0, 7)] + " " + lastNames[casual(0, 7)],
+    coatColor: COAT_COLORS[casual(0, 5)],
+    eyesColor: EYE_COLOR[casual(0, 4)]
+  },
+  {
+    name: names[casual(0, 7)] + " " + lastNames[casual(0, 7)],
+    coatColor: COAT_COLORS[casual(0, 5)],
+    eyesColor: EYE_COLOR[casual(0, 4)]
+  },
+  {
+    name: names[casual(0, 7)] + " " + lastNames[casual(0, 7)],
+    coatColor: COAT_COLORS[casual(0, 5)],
+    eyesColor: EYE_COLOR[casual(0, 4)]
   }
-*/
+];
+
+
+
+for (var i = 0; i <= 3; i++) {
+  wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector(".setup-similar-label").textContent = wizards[i].name;
+  wizardElement.querySelector(".wizard-coat").style.fill = wizards[i].coatColor;
+  wizardElement.querySelector(".wizard-eyes").style.fill = wizards[i].eyesColor;
+  fragment.appendChild(wizardElement);
+}
+elementSimilarList.appendChild(fragment);
