808b4e83f0f15e5c6ea24349536396b549ae2563
Дима <dmitriy.golovin.2001.03@mail.ru>
2019-09-25 13:12:13 +0300

    сделал № 3.

--- js/setup.js
+++ js/setup.js
@@ -2,8 +2,8 @@
 
 var SETUP_ITEM = document.querySelector('.setup');
 SETUP_ITEM.classList.remove('hidden');
-var wizards=[];
-var number_magicians=4;
+var wizards = [];
+var number_magicians = 4;
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var lastNames = ['Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -16,23 +16,22 @@ var setupSimilar = document.querySelector('.setup-similar');
 setupSimilar.classList.remove('hidden');
 
 function getRandomInRange(min, max) {
-return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-for(var i=0; i<number_magicians; i++)
-{
-wizards[i] = {
-name: names[getRandomInRange(0, 7)] + ' ' + lastNames[getRandomInRange(0, 7)],
-coatColor: COAT_COLORS[getRandomInRange(0, 5)],
-eyesColor: COLORS_EYES[getRandomInRange(0, 4)],
-};
+for (var i = 0; i < number_magicians; i++) {
+  wizards[i] = {
+    name: names[getRandomInRange(0, 7)] + ' ' + lastNames[getRandomInRange(0, 7)],
+    coatColor: COAT_COLORS[getRandomInRange(0, 5)],
+    eyesColor: COLORS_EYES[getRandomInRange(0, 4)],
+  };
 }
 
 for (var i = 0; i < wizards.length; i++) {
-wizardElement = similarWizardTemplate.cloneNode(true);
-wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
-fragment.appendChild(wizardElement);
+  wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+  fragment.appendChild(wizardElement);
 }
 elementSimilarList.appendChild(fragment);
