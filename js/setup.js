ad84a4d1855bedafbcc042fee5e84ac080e848fa
Дима <dmitriy.golovin.2001.03@mail.ru>
2019-10-02 15:46:53 +0300

    Исправил ошибку.

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
+var SETUP_OPEN = document.querySelector('.setup-open');
 var wizards = [];
 var WIZARDS_COUNT = 4;
+var wizardEyes = document.querySelector('.wizard-eyes');
+var fireballs = document.querySelector('.setup-fireball-wrap');
 var names = [
   'Иван',
   'Хуан Себастьян',
@@ -44,6 +46,17 @@ var elementSimilarList = document.querySelector('.setup-similar-list');
 var fragment = document.createDocumentFragment();
 var wizardElement;
 var setupSimilar = document.querySelector('.setup-similar');
+var SETUP_CLOSE = setup.querySelector('.setup-close');
+var SETUP_OPEN_ICON = document.querySelector('.setup-open-icon');
+var wizardCoat = document.querySelector('.wizard-coat');
+var fireballColors = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
 setupSimilar.classList.remove('hidden');
 
 function getRandomInRange(min, max) {
@@ -64,3 +77,48 @@ for (var i = 0; i < WIZARDS_COUNT; i++) {
   fragment.appendChild(wizardElement);
 }
 elementSimilarList.appendChild(fragment);
+
+function openWindow() {
+  setup.classList.remove("hidden");
+}
+
+SETUP_OPEN.addEventListener('click', openWindow);
+
+function closeWindow() {
+  setup.classList.add('hidden');
+}
+
+SETUP_CLOSE.addEventListener('click', closeWindow);
+
+SETUP_OPEN_ICON.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    openWindow();
+  }
+});
+
+SETUP_CLOSE.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    closeWindow();
+  }
+
+  if (evt.keyCode === 13) {
+    closeWindow();
+  }
+})
+
+
+function changeColorMantle() {
+  wizardCoat.style.fill = COAT_COLORS[getRandomInRange(0, 5)];
+}
+wizardCoat.addEventListener('click', changeColorMantle);
+
+function changeEyeColor() {
+  wizardEyes.style.fill = COLORS_EYES[getRandomInRange(0, 4)];
+}
+wizardEyes.addEventListener('click', changeEyeColor);
+
+function changeColorFireballs() {
+  console.log(fireballColors[getRandomInRange(0, 4)]);
+  fireballs.style.background = fireballColors[getRandomInRange(0, 4)];
+}
+fireballs.addEventListener('click', changeColorFireballs);
