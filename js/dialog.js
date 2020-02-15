559cc474d8f513956f9bc485008c97ae932f6154
Дима <dmitriy.golovin.2001.03@mail.ru>
2020-02-15 20:04:00 +0300

    Исправил ошибку.

--- js/dialog.js
+++ js/dialog.js
@@ -3,39 +3,35 @@
   var setup = document.querySelector('.setup');
   var setupStyle = getComputedStyle(setup);
   var upload = setup.querySelector('.upload');
-  /* function $(string) {
-    var characterNumber = 0, lastSubstring = 1;
-    var element = document;
-    while (lastSubstring < string.length) {
-      while (string.charAt(characterNumber) === ' ') {
-        characterNumber++;
-        lastSubstring++;
-      }
-      while (string.charAt(characterNumber) !== ' ' && lastSubstring < string.length) {
-        lastSubstring++;
-      }
-      element = element.querySelector(string.substring(characterNumber, lastSubstring - 1));
-    }
-    return element;
-  }*/
   upload.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
     var coordinate = {
       x: evt.clientX,
       y: evt.clientY
     };
-
+    var dragged=false;
     document.addEventListener('mousemove', onDocumentMousemove);
     function onDocumentMousemove(moveEvt) {
       moveEvt.preventDefault();
+      dragged=true;
       setup.style.marginTop = Number.parseInt(setupStyle.marginTop, 10) + (moveEvt.clientY - coordinate.y) + 'px';
       setup.style.marginLeft = Number.parseInt(setupStyle.marginLeft, 10) + (moveEvt.clientX - coordinate.x) + 'px';
       coordinate.y = moveEvt.clientY;
       coordinate.x = moveEvt.clientX;
     }
-    document.addEventListener('mouseup', function (defaultEvent) {
-      defaultEvent.preventDefault();
+    document.addEventListener('mouseup',onMouseup);
+    function onMouseup(defaultEvent) {
+      //defaultEvent.preventDefault();
       document.removeEventListener('mousemove', onDocumentMousemove);
-    });
+      document.removeEventListener('mouseup',onMouseup);
+      if(dragged===true)
+      {
+       upload.addEventListener('click',onUploadClick);
+       function onUploadClick (evt) {
+         evt.preventDefault();
+         uload.removeEventListener('click',onUloadClick);
+       }
+      }
+    }
   });
 })();
